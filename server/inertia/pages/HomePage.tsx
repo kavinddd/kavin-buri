0b704b78078d5f747512a5588785c7902afcaa59
Win <fnvin@hotmail.com>
2025-02-18 00:30:01 +0700

    feat(sv): add home page animation

--- server/inertia/pages/HomePage.tsx
+++ server/inertia/pages/HomePage.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
     <>
       <Head title="Home" />
       <MainLayout backgroundSrc={'/images/hero-home.jpg'}>
-        <p className="flex justify-center align-center h-full text-3xl text-primary-foreground">
+        <p className="animate-fade-in-up flex justify-center align-center h-full text-3xl text-primary-foreground">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
           ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
