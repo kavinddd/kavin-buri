1b3ac68f7100740ea405976980ea87d3674719a3
Win <fnvin@hotmail.com>
2025-02-18 00:29:15 +0700

    feat(sv): add more shadcn components

---
+++ server/inertia/components/DateRangePicker.tsx
@@ -0,0 +1,42 @@
+import { Popover, PopoverContent, PopoverTrigger } from '@radix-ui/react-popover'
+import { format } from 'date-fns'
+import { CalendarIcon } from 'lucide-react'
+import { useState } from 'react'
+import { Button } from '~/lib/components/ui/button'
+import { cn } from '~/lib/lib/utils'
+import { Calendar } from '~/lib/components/ui/calendar'
+import { DateRange, SelectRangeEventHandler } from 'react-day-picker'
+
+type DateRangePickerProps = {
+  placeholder: string
+  value: DateRange | undefined
+  onChange: SelectRangeEventHandler
+  showFrom?: boolean
+  showTo?: boolean
+}
+export function DateRangePicker({
+  placeholder = 'Pick a date',
+  value,
+  onChange,
+  showFrom = true,
+  showTo = true,
+}: DateRangePickerProps) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn('justify-start text-left font-normal', !value && 'text-muted-foreground')}
+        >
+          {!value && placeholder}
+          {showFrom && !showTo && value?.from && format(value.from, 'PPP')}
+          {showTo && !showFrom && value?.to && format(value.to, 'PPP')}
+          <CalendarIcon className="mr-2 h-4 w-4" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0 border-secondary border-2 bg-zinc-200">
+        <Calendar mode="range" selected={value} onSelect={onChange} initialFocus />
+      </PopoverContent>
+    </Popover>
+  )
+}
--- server/tailwind.config.js
+++ server/tailwind.config.js
@@ -8,6 +8,39 @@ export default {
   ],
   theme: {
   	extend: {
+  		animation: {
+  			'fade-in-up': 'fade-in-up 1s ease-in-out',
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		keyframes: {
+  			'fade-in-up': {
+  				'0%': {
+  					opacity: 0,
+  					transform: 'translate3d(0, 100%, 0)'
+  				},
+  				'100%': {
+  					opacity: 1,
+  					transform: 'translate3d(0, 0, 0)'
+  				}
+  			},
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
   		fontFamily: {
   			inter: [
   				'Inter',
