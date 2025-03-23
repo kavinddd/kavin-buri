1bcf656bd24a306ffe9c824248c214ce24631037
Win <fnvin@hotmail.com>
2025-03-23 15:23:56 +0700

    feat(server): add static table name for booking guest

--- server/app/models/booking_guest.ts
+++ server/app/models/booking_guest.ts
@@ -3,6 +3,7 @@ import type { BookingId } from './booking.js'
 import type { GuestId } from './guest.js'
 
 export default class BookingGuest extends BaseModel {
+  static table = 'booking_guest'
   @column({ isPrimary: true })
   declare bookingId: BookingId
 
