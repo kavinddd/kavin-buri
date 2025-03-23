21439b45b55da85045909074f97a07ef418ecc25
Win <fnvin@hotmail.com>
2025-03-23 15:17:59 +0700

    fix(bo): redirect not found page

--- backoffice/src/components/errors/NotFoundPage.tsx
+++ backoffice/src/components/errors/NotFoundPage.tsx
@@ -1,3 +1,10 @@
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+
 export default function NotFoundPage() {
-  return <p>Page Not Found</p>;
+  const navigate = useNavigate();
+
+  navigate("/");
+  toast.error("Page was not found");
+  return null;
 }
